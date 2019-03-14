46bb79ce7b1c12af298c2d8fe84926381901bdcf
Julia Zajusz <julia.zajusz@poczta.fm>
2019-03-14 16:22:07 +0100

    init debugger

---
+++ etap1/adder.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    const int a = 1;
+    const int b = 2;
+
+    int result = a + b;
+
+    int breakpoint = 1;
+
+    printf("%d\n",result);
+
+    return 0;
+}
