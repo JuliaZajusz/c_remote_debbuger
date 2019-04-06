738e48c008944e71ae15f6ec3ebc21d3a9fb4296
julia <julia.zajusz@ringosolutions.com>
2019-04-06 13:02:13 +0200

    webapp - waiting loop & relative path fix;

--- c_remote_debugger-app/cpp/run.sh
+++ c_remote_debugger-app/cpp/run.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
 
 if [[ "$(docker inspect --type=image minidbg_image:latest)" == "[]" ]]; then
   echo "create image minidbg_image"
-  docker build . -t minidbg_image
+  docker build ./../cpp/ -t minidbg_image
 fi
+
+while [[ "$(docker inspect --type=image minidbg_image:latest)" == "[]" ]]
+do
+  sleep 10
+  echo "waiting for image..."
+done
 
 number=1
 while [ "$(docker inspect --type=container minidbg_container${number})" != "[]" ]
