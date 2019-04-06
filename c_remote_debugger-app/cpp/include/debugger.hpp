d0a71f2d6729b3ecc89d94ae388ff686abb8a8d7
julia <julia.zajusz@ringosolutions.com>
2019-04-06 08:44:55 +0200

    fix build

--- c_remote_debugger-app/cpp/include/debugger.hpp
+++ c_remote_debugger-app/cpp/include/debugger.hpp
@@ -1,14 +1,15 @@
 #ifndef MINIDBG_DEBUGGER_HPP
 #define MINIDBG_DEBUGGER_HPP
 
-#include <utility>
-#include <string>
-#include <linux/types.h>
 #include <unordered_map>
+#include <cstdint>
+#include <signal.h>
+#include <fcntl.h>
 
-#include "breakpoint.hpp"
 #include "dwarf/dwarf++.hh"
 #include "elf/elf++.hh"
+
+#include "breakpoint.hpp"
 
 namespace minidbg {
     enum class symbol_type {
@@ -19,18 +20,13 @@ namespace minidbg {
         file,              // Source file associated with the
     };                     // object file
 
-    std::string to_string(symbol_type st) {
+    std::string to_string (symbol_type st) {
         switch (st) {
-            case symbol_type::notype:
-                return "notype";
-            case symbol_type::object:
-                return "object";
-            case symbol_type::func:
-                return "func";
-            case symbol_type::section:
-                return "section";
-            case symbol_type::file:
-                return "file";
+            case symbol_type::notype: return "notype";
+            case symbol_type::object: return "object";
+            case symbol_type::func: return "func";
+            case symbol_type::section: return "section";
+            case symbol_type::file: return "file";
         }
     }
 
@@ -51,57 +47,40 @@ namespace minidbg {
         }
 
         void run();
-        void set_breakpoint_at_address(std::intptr_t addr);
-
-        void set_breakpoint_at_function(const std::string &name);
-
-        void set_breakpoint_at_source_line(const std::string &file, unsigned line);
         void dump_registers();
-
-        void print_backtrace();
-
-        void read_variables();
-
-        void print_source(const std::string &file_name, unsigned line, unsigned n_lines_context = 2);
-
-        auto lookup_symbol(const std::string &name) -> std::vector<symbol>;
-
-        void single_step_instruction();
-
-        void single_step_instruction_with_breakpoint_check();
-
-        void step_in();
-
-        void step_over();
-
-        void step_out();
-
-        void remove_breakpoint(std::intptr_t addr);
-
-    private:
-        void handle_command(const std::string& line);
-        void continue_execution();
-        auto get_pc() -> uint64_t;
-        void set_pc(uint64_t pc);
-        void step_over_breakpoint();
-        void wait_for_signal();
-
-        auto get_signal_info() -> siginfo_t;
-
-        void handle_sigtrap(siginfo_t info);
-
-        auto get_function_from_pc(uint64_t pc) -> dwarf::die;
-
-        auto get_line_entry_from_pc(uint64_t pc) -> dwarf::line_table::iterator;
-
         auto read_memory(uint64_t address) -> uint64_t;
         void write_memory(uint64_t address, uint64_t value);
+        void print_backtrace();
+        void read_variables();
+        void continue_execution();
+        void single_step_instruction_with_breakpoint_check();
+        void step_in();
+        void step_out();
+        void step_over();
+        auto get_signal_info() -> siginfo_t;
+        void remove_breakpoint(std::intptr_t addr);
+        void set_breakpoint_at_function(const std::string& name);
+        void set_breakpoint_at_address(std::intptr_t addr);
+        void set_breakpoint_at_source_line(const std::string& file, unsigned line);
+        void print_source(const std::string& file_name, unsigned line, unsigned n_lines_context=2);
+        auto lookup_symbol(const std::string&) -> std::vector<symbol>;
+
+    private:
+        void single_step_instruction(); //single step without checking breakpoints
+        void step_over_breakpoint();
+        void handle_command(const std::string& line);
+        void handle_sigtrap(siginfo_t info);
+        void wait_for_signal();
+        auto get_pc() -> uint64_t;
+        void set_pc(uint64_t pc);
+        auto get_line_entry_from_pc(uint64_t pc) -> dwarf::line_table::iterator;
+        auto get_function_from_pc(uint64_t pc) -> dwarf::die;
 
         std::string m_prog_name;
         pid_t m_pid;
-        std::unordered_map<std::intptr_t, breakpoint> m_breakpoints;
         dwarf::dwarf m_dwarf;
         elf::elf m_elf;
+        std::unordered_map<std::intptr_t,breakpoint> m_breakpoints;
     };
 }
 
